66f7619cbab07fdcbcec1b55feea897ff3b2758b
JaretWright <hkkA223>
2017-09-14 15:50:44 -0400

    Added comparison class

---
+++ src/f17sept14/Comparisons.java
@@ -0,0 +1,17 @@
+package f17sept14;
+
+/**
+ *
+ * @author JWright
+ */
+public class Comparisons {
+    
+    public static void main(String[] args)
+    {
+        System.out.printf("5 > 10 results in: %s %n", 5>10);
+        System.out.printf("5 < 10 results in: %s %n", 5<10);
+        System.out.printf("5 <= 10 results in: %s %n", 5<=10);
+        System.out.printf("5 == 10 results in: %s %n", 5==10);
+    }
+   
+}
